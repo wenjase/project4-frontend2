62b9bfcafefacc8dd8373011b017b71244cac6ea
alec2zac <alecjohnson708@gmail.com>
2021-10-16 11:05:49 -0500

    got it up and running

---
+++ q_a/src/components/Answer/Answer.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import React from 'react';
+
+
+function Answer(props) {
+
+    const [answer, setAnswer] = useState("")
+    const [answers, setAnswers] = useState([]) 
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetch("http://localhost:8000/answers/", {
+            "headers": {
+                "content-type": "application/json",
+            },
+            "body": JSON.stringify({answer:answer}),
+            "method": "POST",
+        })
+    }
+
+
+    useEffect(() => {
+        fetch('http://localhost:8000/answers/')
+        .then(res => res.json())
+        .then(res => {
+            setAnswers(res)
+            console.log(res)
+        })
+    
+    }, [])
+
+
+const handleChange = (e) => {
+    setAnswer(e.target.value)
+}
+
+
+    return (
+        <div>
+             <form onSubmit={handleSubmit}>
+                <input name='answer' type='text' onChange={handleChange} />
+                <input type='submit' />
+            </form>
+            <div className="answer">
+                    {answers && (
+                        answers.map((answer) => {
+                            return (<h2>{answer.answer}</h2>)
+                        })
+                    )}
+                </div>
+        </div>
+    );
+}
+
+export default Answer;
