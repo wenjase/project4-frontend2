176415ba223e55f7a98da6c7b4b43d94267bf394
wenjase <alecjohnson708@gmail.com>
2021-10-18 09:19:33 -0500

    hello

--- q_a/src/components/Routes.js
+++
@@ -1,7 +0,0 @@
-export {default as Nav} from "./Nav/Nav"
-export {default as Footer} from "./Footer/Footer"
-export {default as About} from "./About/About"
-export {default as Contact} from "./Contact/Contact"
-export {default as Home} from "./Home/Home"
-export {default as Answer} from "./Answer/Answer"
-export {default as Login} from "./Login/Login"
