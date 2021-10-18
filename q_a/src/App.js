aeb5830fed2ebbfa89962461895377518599bc5c
wenjase <alecjohnson708@gmail.com>
2021-10-18 16:33:47 -0500

    initial commit

---
+++ q_a/src/App.js
@@ -0,0 +1,23 @@
+import {Nav, Footer, About, Contact, Home, Answer, Login} from "./components/Routes"
+import { Route,Switch } from 'react-router-dom'
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Switch>
+        <Route path='/' render={() => 
+        <Fragment>
+          <Home />
+          <Answer />
+          </Fragment>} />
+        <Route path='/about' exact component={() => <About />} />
+        <Route path='/contact' exact component={() => <Contact />} />
+      </Switch>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ q_a/src/components/Routes.js
@@ -0,0 +1,7 @@
+export {default as Nav} from "./Nav/Nav"
+export {default as Footer} from "./Footer/Footer"
+export {default as About} from "./About/About"
+export {default as Contact} from "./Contact/Contact"
+export {default as Home} from "./Home/Home"
+export {default as Answer} from "./Answer/Answer"
+export {default as Login} from "./Login/Login"
